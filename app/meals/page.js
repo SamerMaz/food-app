81632e8eb1e7492ce376febcc13f066809471a03
SamerMaz <samer.mazraani96@gmail.com>
2024-06-22 16:25:37 +0300

    adding mealsPage + meals database

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,10 +1,28 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+export default async function MealsPage() {
+// because this is a server component we can reach the database from directly from here
 
-export default function MealsPage() {
+  const meals = await getMeals();
   return (
-    <div>
-      <h1>Meals</h1>
-      <Link href={"/meals/meals_1"}>Meal 1</Link>
-    </div>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share you favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals}/>
+      </main>
+    </>
   );
 }
